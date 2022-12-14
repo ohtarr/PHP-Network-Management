9d97efcd38c2da36d140c0c7e585d381ee5c46f9
Ohtarr <ohtarr@gmail.com>
2022-12-14 17:04:29 -0600

    Optimizing processTcpDump

--- app/Console/Commands/processTcpdump.php
+++ app/Console/Commands/processTcpdump.php
@@ -37,7 +37,36 @@ class processTcpdump extends Command
      */
     public function handle()
     {
+        $INCREMENT = 300;
+        $TIME = time(); // Get our start time, we rotate the IPS array every 60 secondsish.
+
         while ( !feof(STDIN) )                  // Loop while there is a STDIN stream
+        {
+            $LINE = fgets(STDIN, 8192);     // Get a line of input to process
+            $REGEX = "/^\S+\s+IP\s+(\d+\.\d+\.\d+\.\d+)\.\d+\s>\s\d+\.\d+\.\d+\.\d+\.\d+:\s.+$/";
+            if ( preg_match($REGEX,$LINE,$MATCH) )
+            {
+                // process the output:
+                // Increment the number of hits we have seen this ip for THIS TIME INTERVAL!
+                if(!isset($IPS[$MATCH[1]]))
+                { 
+                    $IPS[$MATCH[1]] = 1;
+                }
+            }
+            // Process rotation of the IPS for each time interval
+            $NOW = time();
+            if ($NOW > $TIME + $INCREMENT)  // If we are outside the time interval, rotate the IPS array!
+            {
+                foreach($IPS as $ip => $hits)
+                {
+                    $this->call('netman:AutoDiscoverDevice',['--ip' => $ip]);
+                }
+                $IPS = [];
+                $TIME = $NOW;
+            }
+        }
+
+/*         while ( !feof(STDIN) )                  // Loop while there is a STDIN stream
         {
             $LINE = fgets(STDIN, 8192);     // Get a line of input to process
             $REGEX = "/IP\s+(\d+\.\d+\.\d+\.\d+)\.\d+\s>\s\d+\.\d+\.\d+\.\d+\.\d+/";
@@ -45,6 +74,6 @@ class processTcpdump extends Command
             {
                 $this->call('netman:AutoDiscoverDevice',['--ip' => $MATCH[1]]);
             }
-        }
+        } */
     }
 }
