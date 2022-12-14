a199f7e31ec7a117644e06b9a90fa225f205dfea
Ohtarr <ohtarr@gmail.com>
2022-12-14 16:32:59 -0600

    remove unneeded Observium Database

--- config/database.php
+++ config/database.php
@@ -63,26 +63,6 @@ return [
             ]) : [],
         ],
 
-        'mysql-observium' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE2_URL'),
-            'host' => env('DB2_HOST', '127.0.0.1'),
-            'port' => env('DB2_PORT', '3306'),
-            'database' => env('DB2_DATABASE', 'forge'),
-            'username' => env('DB2_USERNAME', 'forge'),
-            'password' => env('DB2_PASSWORD', ''),
-            'unix_socket' => env('DB2_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
         'mysql-whereuat' => [
             'driver' => 'mysql',
             'url' => env('DATABASE3_URL'),
