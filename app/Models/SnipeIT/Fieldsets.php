acf7cb6465da6290469ffa141a3388fd48b49fa7
unknown <ohtarr@gmail.com>
2026-06-02 12:04:26 -0500

    Added SnipeIT Fieldsets model

---
+++ app/Models/SnipeIT/Fieldsets.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\SnipeIT;
+
+use App\Models\SnipeIT\BaseModel;
+
+#[\AllowDynamicProperties]
+class Fieldsets extends BaseModel
+{
+    protected static $snipeitmodel = "fieldsets";
+
+}
