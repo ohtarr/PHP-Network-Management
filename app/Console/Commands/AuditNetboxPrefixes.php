9a984763feb6800f2f4649d1e5a7c2c2a0c08fa4
unknown <ohtarr@gmail.com>
2026-06-12 13:04:46 -0500

    Modified AuditNetboxPrefixes

--- app/Console/Commands/AuditNetboxPrefixes.php
+++ app/Console/Commands/AuditNetboxPrefixes.php
@@ -13,7 +13,7 @@ class AuditNetboxPrefixes extends Command
      *
      * @var string
      */
-    protected $signature = 'netman:AuditNetboxPrefixes';
+    protected $signature = 'netman:AuditNetboxPrefixes {--fix}';
 
     /**
      * The console command description.
@@ -22,6 +22,10 @@ class AuditNetboxPrefixes extends Command
      */
     protected $description = 'Audit ACTIVE prefixes for site supernets.';
 
+    public $sites;
+    public $prefixes;
+    public $failed;
+    public $tomake;
 
     /**
      * Execute the console command.
@@ -30,29 +34,87 @@ class AuditNetboxPrefixes extends Command
      */
     public function handle()
     {
-        $sites = Sites::all();
-        $prefixes = Prefixes::all();
-        foreach($sites as $site)
+        $fix = $this->option('fix');
+        $this->AuditNetboxPrefixes();
+        print "To Make: " . PHP_EOL;
+        print_r($this->tomake);
+        print "Failed to generate networks: " . PHP_EOL;
+        print_r($this->failed);
+        if($fix)
         {
-            unset($generated);
-            //$supernet = $site->getProvisioningSupernet();
-            $generated = $site->generateSiteNetworks();
-            if(!$generated)
+            $this->createNetboxPrefixes();
+        }
+    }
+
+    public function getSites()
+    {
+        if(!$this->sites)
+        {
+            $this->sites = Sites::all();
+        }
+        return $this->sites;
+    }
+
+    public function getPrefixes()
+    {
+        if(!$this->prefixes)
+        {
+            $this->prefixes = Prefixes::all();
+        }
+        return $this->prefixes;
+    }
+
+    public function AuditNetboxPrefixes()
+    {
+        if(!$this->tomake)
+        {
+            $failed = [];
+            $tomake = [];
+            $sites = $this->getSites();
+            $prefixes = $this->getPrefixes();
+            foreach($sites as $site)
             {
-                $failed[] = $site->name;
-                continue;
-            }
-            foreach($generated as $vlan => $genprefix)
-            {
-                $prefix = $prefixes->where('prefix', $genprefix['network'] . "/" . $genprefix['bitmask'])->first();
-                if(!$prefix)
+                unset($generated);
+                unset($tmp);
+                $vlans = [];
+                $generated = $site->generateSiteNetworks();
+                if(!$generated)
                 {
-                    $tomake[$site->name][] = $vlan;
+                    $failed[] = $site->name;
+                    continue;
+                }
+                foreach($generated as $vlan => $genprefix)
+                {
+                    $prefix = $prefixes->where('prefix', $genprefix['network'] . "/" . $genprefix['bitmask'])->first();
+                    if(!$prefix)
+                    {
+                        $vlans[] = $vlan;
+                    }
+                }
+                if($vlans)
+                {
+                    $tmp['site'] = $site;
+                    $tmp['vlans'] = $vlans;
+                    $tomake[] = $tmp;
                 }
             }
+            $this->failed = $failed;
+            $this->tomake = $tomake;
         }
-        print_r($failed);
-        print_r($tomake);
+        return $this->tomake;
+    }
+
+    public function createNetboxPrefixes()
+    {
+        foreach($this->tomake as $sitetomake)
+        {
+            $site = $sitetomake['site'];
+            foreach($sitetomake['vlans'] as $vlanid)
+            {
+                print "Deploying PREFIX for vlan {$vlanid} for site {$site->name}..." . PHP_EOL;
+                $site->deployActivePrefix($vlanid);
+            }
+        }
     }
 
 }
