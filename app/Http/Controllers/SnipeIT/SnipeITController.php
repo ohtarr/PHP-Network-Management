0e6b91742ec504746b6bf33f66a3457d68ab729e
unknown <ohtarr@gmail.com>
2026-07-27 08:44:50 -0500

    forgot to re-enable auth on SnipeItController on last commit

--- app/Http/Controllers/SnipeIT/SnipeITController.php
+++ app/Http/Controllers/SnipeIT/SnipeITController.php
@@ -20,7 +20,7 @@ class SnipeITController extends Controller
 
     public function __construct()
     {
-	    //$this->middleware('auth:api');
+	    $this->middleware('auth:api');
     }
 
     public function addLog($status, $msg)
