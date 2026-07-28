6e7f40bc980ad7e0652d808c6ef7e3e2d66eea6c
unknown <ohtarr@gmail.com>
2026-07-28 13:53:38 -0500

    Forgot to enable auth on SitesController

--- app/Http/Controllers/Netbox/Sites/SitesController.php
+++ app/Http/Controllers/Netbox/Sites/SitesController.php
@@ -10,7 +10,7 @@ class SitesController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     /**
